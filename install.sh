f97fb5c7ea8202d8e312762396fe49f549d51ea5
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-06-03 09:42:09 -0400

    chore: Update installation script for inFlex

--- install.sh
+++ install.sh
@@ -1,15 +1,37 @@
+#!/bin/bash
+
+# Clonar el repositorio
 git clone https://github.com/HenryQuispeHuayta/interprete-pseudocodigo.git ~/interprete-pseudocodigo
-cd ~/interprete-pseudocodigo
-if [ -f ~/.bashrc ]; then
-    echo "alias inFlex='python $(pwd)/inFlex.py'" >> ~/.bashrc
-elif [ -f ~/.zshrc ]; then
-    echo "alias inFlex='python $(pwd)/inFlex.py'" >> ~/.zshrc
-else
-    echo "No se pudo encontrar ni ~/.bashrc ni ~/.zshrc"
+
+# Verificar si se clonó correctamente
+if [ $? -ne 0 ]; then
+    echo "Error: No se pudo clonar el repositorio."
     exit 1
 fi
-if [ -f ~/.bashrc ]; then
-    source ~/.bashrc
+
+# Definir el comando a utilizar
+command="python ~/interprete-pseudocodigo/inFlex.py"
+
+# Agregar alias a ~/.zshrc si se está utilizando Zsh
+if [ -f ~/.zshrc ]; then
+    if ! grep -q "alias inFlex='$command'" ~/.zshrc; then
+        echo "alias inFlex='$command'" >> ~/.zshrc
+        echo "Alias agregado a ~/.zshrc"
+    else
+        echo "El alias 'inFlex' ya está definido en ~/.zshrc."
+    fi
+# Agregar alias a ~/.bashrc si no se está utilizando Zsh
+elif [ -f ~/.bashrc ]; then
+    if ! grep -q "alias inFlex='$command'" ~/.bashrc; then
+        echo "alias inFlex='$command'" >> ~/.bashrc
+        echo "Alias agregado a ~/.bashrc"
+    else
+        echo "El alias 'inFlex' ya está definido en ~/.bashrc."
+    fi
 else
-    source ~/.zshrc
+    echo "No se pudo encontrar ni ~/.zshrc ni ~/.bashrc"
+    exit 1
 fi
+
+# Recargar el archivo de configuración actual
+source ~/.zshrc || source ~/.bashrc
